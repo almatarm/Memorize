646b833080ed85a5714e5bd29d8ffaa4aac82579
Mufeed Matar <almatarm@gmail.com>
2023-03-06 08:54:58 +0300

    init commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mufeed AlMatar on 14/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
